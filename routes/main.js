fbfe42ebf282dbf0f8e97a13bf3b74dd7184d9b2
130N DEVELOPER <65381159+130N-STUDIO@users.noreply.github.com>
2021-08-05 12:51:15 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -16,10 +16,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'XYZbot',
+            namaowner: 'LionChouu',
+            instagram: 'thisleonreal',
+            youtube : 'thisleonreal'
         }
     }
     res.json(config)
