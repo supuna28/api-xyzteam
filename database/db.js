fa1b3c962c5a32dfea3a000d55799e206e86d556
130N DEVELOPER <65381159+130N-STUDIO@users.noreply.github.com>
2021-08-05 13:07:17 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,16 +3,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://zahirr-web.herokuapp.com';
+var url = 'https://xyz-restapikey.herokuapp.com';
 try {
-if(url == 'https://zahirr-web.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://xyz-restapikey.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, ZhirrrGanss','green'))
+  console.log(color('Connected correctly to server, ThisLeonReal','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
