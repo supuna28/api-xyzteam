11a88e5973772f2aaf32a1b9d0a9e053aafa8d00
130N DEVELOPER <65381159+130N-STUDIO@users.noreply.github.com>
2021-08-20 09:31:38 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/home.html')
+    res.sendFile(__path + '/views/maintance/index.html')
 })
 
 router.get('/register', (req, res) => {
